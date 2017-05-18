171c0283da0732bad5dd7913023159edfae151d5
NinakoLovu <leehtironi@gmail.com>
2017-05-17 21:21:39 -0300

    just testing a new process.

---
+++ wtf.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "user.h"
+
+
+int main(void){
+   printf(1, "working...!?\n");
+
+
+   exit();
+}
---
+++ wtf.d
@@ -0,0 +1,1 @@
+wtf.o: wtf.c /usr/include/stdc-predef.h types.h user.h
